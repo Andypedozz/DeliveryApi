a3da50a88beb8e8779c6a075210370b069d5f12a
andrea <andypedini02@gmail.com>
2025-10-08 15:11:13 +0200

    .

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const sqlite3 = require("sqlite3").verbose()
 const path = require("path")
 const { fetchFirst, fetchAll, execute } = require("./db/db_wrapper.js"); 
@@ -7,6 +8,7 @@ const app = express();
 
 const PORT = 5000;
 
+app.use(cors())
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "/")));
 
